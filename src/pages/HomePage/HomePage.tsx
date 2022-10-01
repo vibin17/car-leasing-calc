9d68663307cc2e4fb2d917c1b004882732ecc2e0
Danila Grachev <danyagrachev@gmail.com>
2022-10-01 20:02:35 +0300

    upd

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import './HomePage.scss'
+
+const HomePage = () => {
+    return (
+        <div className="home">
+            <h1 className="title">
+                Рассчитайте стоимость автомобиля в лизинг
+            </h1>
+        </div>
+    )
+}
+
+export default HomePage
